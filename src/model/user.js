ed0858bcae2ec8e209a3491ca26355691a9213ee
Jamil <jamilahmedchowdhury4128@gmail.com>
2020-08-21 07:14:47 +0600

    hiding user

--- src/model/user.js
+++ src/model/user.js
@@ -52,6 +52,13 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
+}
 //generateToken
 userSchema.methods.generateAuthToken = async function () {
     const user = this
