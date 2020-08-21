ea58d82fb685302afbe4bd679f1efab8cf24bf45
Jamil <jamilahmedchowdhury4128@gmail.com>
2020-08-21 06:43:35 +0600

    logout

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -21,6 +21,27 @@ router.post("/users/login", async (req, res) => {
         res.send(error)
     }
 })
+router.post("/users/logout", auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((tok) => {
+            return tok.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+router.post("/users/logoutAll", auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.status(200).send("Logged out of all session")
+    } catch (error) {
+        res.status(500).send()
+
+    }
+})
 router.get("/users/me", auth, async (req, res) => {
     res.send(req.user)
 })
